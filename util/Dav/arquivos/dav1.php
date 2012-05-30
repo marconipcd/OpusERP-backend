0baf2f87aa1815dd4eff262bb010b9b6391f93a4
Marconi Cesaar <marconipcd@gmail.com>
2012-05-30 17:06:20 -0300

    Concluido PDF DAV

--- util/Dav/arquivos/dav1.php
+++ util/Dav/arquivos/dav1.php
@@ -8,14 +8,20 @@ header("Content-Type: text/html; charset=ISO-8859-1",true);
     {
         $nDAV = $_GET['nDAV'];
         
+	
+	
         
-        //PROCURA ITENS DO DAV
-        
+        //PROCURA ITENS DO DAV        
 	$query0 = "SELECT * 
                 FROM  dav_detalhe WHERE NUMERO_DAV LIKE '$nDAV'";
 	$result0 = $conn->query($query0);
         
-        
+        //PROCURAR DAV CABECALHO E INFORMACOES DA EMPRESA E DO CLIENTE DESTINATARIO
+	$query1 = "SELECT dc.*, e.* FROM dav_cabecalho as dc, 
+	empresa as e WHERE dc.EMPRESA_ID = e.ID AND dc.ID = $nDAV";
+	
+	$result1 = $conn->query($query1);
+	$row1 = $result1->fetch_assoc();
         
         
     }
@@ -38,13 +44,13 @@ header("Content-Type: text/html; charset=ISO-8859-1",true);
       <p>NAO E DOCUMENTO FISCAL - NAO E VALIDO COMO RECIBO E COMO GARANTIA DE MERCADORIA - NAO COMPROVA PAGAMENTO</p>
       <br/>
       <p>Identificacao do Estabelecimento Emitente :</p>
-      <p>OK MICRO LTDA</p>
-      <p>RUA DEMOCRITO DE SOUZA FILHO 503  - MADALENA - RECIFE - PE  FONE : (81)3222-9197    (81)3031-7260</p>
-      <p>CNPJ : 05.044.579/0001-08</p>
+      <p><?php echo($row1['RAZAO_SOCIAL'])?></p>
+      <p><?php echo($row1['ENDERECO'])?>, <?php echo($row1['BAIRRO'])?>, <?php echo($row1['CIDADE'])?>, <?php echo($row1['UF'])?>  - <?php echo($row1['FONE1'])?> <?php echo(' '.$row1['FONE2'])?></p>
+      <p>CNPJ : <?php echo($row1['CNPJ'])?></p>
       <br/>
       <p>Identificação do Destinatário :</p>
-      <p>ADEMIR DE SOUZA PINTO FILHO</p>
-      <p>CNPJ : 07.578.965/0001-05</p>
+      <p><?php echo($row1['NOME_DESTINATARIO'])?></p>
+      <p>CPF/CNPJ : <?php echo($row1['CPF_CNPJ_DESTINATARIO'])?></p>
       <br/>
       <br/>
       <br/>
@@ -54,7 +60,7 @@ header("Content-Type: text/html; charset=ISO-8859-1",true);
       
       <table width="100%">
         <tr>
-            <td width="300">Nº do Documento : 0000007908</td>
+            <td width="300">Nº do Documento : <?php echo($nDAV)?></td>
             <td>Nº do Documento Fiscal : _____________________</td>
         </tr>        
       </table>
@@ -64,10 +70,10 @@ header("Content-Type: text/html; charset=ISO-8859-1",true);
       
         <tr>
             <td width="80">Codigo</td>
-            <td width="300">Produto</td>
+            <td width="250">Produto</td>
             <td width="60">Quant.</td>
-            <td width="60">Preco</td>
-            <td width="60">SubTotal</td>
+            <td width="70">Preco</td>
+            <td width="70">SubTotal</td>
             <td width="60">Desconto</td>
             <td width="60">Total</td>
         </tr>
@@ -103,6 +109,32 @@ header("Content-Type: text/html; charset=ISO-8859-1",true);
         <tr>
             <td colspan="7" width="680">
                 <hr width="680" />                
+            </td>
+         </tr>
+	 
+	
+	 
+	 <tr>
+            <td colspan="7" width="680">
+               <br/><br/><br/>       
+            </td>
+         </tr>
+	 
+	 <tr>
+            <td colspan="7" width="680">
+               DESCONTO:  <?php echo($row1['DESCONTO'])?>        
+            </td>
+         </tr>
+	 
+	 <tr>
+            <td colspan="7" width="680">
+               ACRESCIMO:  <?php echo($row1['ACRESCIMO'])?>        
+            </td>
+         </tr>
+	 
+	 <tr>
+            <td colspan="7" width="680">
+               TOTAL:  <?php echo($row1['SUBTOTAL'])?>        
             </td>
          </tr>
        
